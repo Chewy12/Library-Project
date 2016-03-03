023e0e994446e0e45fcc9301a88da43c9cf9f011
Chewy12 <leech5@bxscience.edu>
2016-03-02 23:04:24 -0500

    Update Book.java

--- Library Management System/Book.java
+++ Library Management System/Book.java
@@ -1,3 +1,24 @@
 public class Book{
-  
+  private String isbn;
+  private String title;
+  private String author;
+  private String conditions;
+  public Book(String isbn, String title, String author, String conditions){
+    isbn = isbn;
+    title = titleIn;
+    author = authorIn;
+    condition = conditionIn;
+  }
+  public String getIsbn(){
+    return isbn;
+  } 
+  public String getTitle(){
+    return titleIn;
+  }
+  public String getAuthor(){
+    return authorIn;
+  }
+  public String getConditions(){
+    return conditionIn;
+  }
 }
